0d608372a0877010ab3fedde96994492f019bb17
Amrita977. <Amritarajbansh@gmail.com>
2025-06-04 21:55:32 +0530

    updated blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,12 @@
-<h1>Blog Page</h1>
-<h2>My Blog Page</h2>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Blog Page</title>
+</head>
+<body>
+    <h1>Blog Page</h1>
+    <h2>My Blog Page</h2>
 
-<a href="{{route('blog')}}">Home</a>
+    <a href="{{ route('blog') }}">Home</a>
+</body>
+</html>
