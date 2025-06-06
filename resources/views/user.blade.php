a492149dae3e9c8017e6b0a3f204a2277d937e64
Amrita977. <Amritarajbansh@gmail.com>
2025-06-06 23:30:32 +0530

    updated user blade.

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,2 +1,12 @@
-<h1>User Page</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Page</title>
+</head>
+<body>
+    <h1>User Page</h1>
+    <h2>User Page</h2>
 
+    <a href="{{ route('user') }}">Home</a>
+</body>
+</html>
